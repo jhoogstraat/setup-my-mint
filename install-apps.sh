d08b4cec6a0050074e4e682b2c939b827bd59789
Joshua Hoogstraat <34964599+jhoogstraat@users.noreply.github.com>
2020-07-23 12:30:01 +0200

    Added PDFsam Visual

--- install-apps.sh
+++ install-apps.sh
@@ -25,11 +25,17 @@ code --install-extension gruntfuggly.todo-tree
 wget "https://updates.insomnia.rest/downloads/ubuntu/latest" -O insomnia.deb
 sudo apt install ./insomnia.deb
 
+# Android Studio
 wget "https://r4---sn-8xgn5uxa-i5he.gvt1.com/edgedl/android/studio/ide-zips/4.0.1.0/android-studio-ide-193.6626763-linux.tar.gz?cms_redirect=yes&mh=Ft&mip=89.27.237.90&mm=28&mn=sn-8xgn5uxa-i5he&ms=nvh&mt=1595249599&mv=m&mvi=4&pl=24&shardbypass=yes" -O android_studio.tar.gz
 mkdir /opt/google
 tar xf android_studio.tar.gz 
 sudo mv android-studio /opt/google/android-studio
 
+# PDFSam Visual
+wget "https://github.com/torakiki/pdfsam-visual-public/releases/download/v2.1.7/pdfsam-visual_2.1.7_amd64.deb"
+sudo apt install ./pdfsam-visual_2.1.7_amd64.deb
+mkdir ~/.config/PDFsam\ Visual
+echo '{"autoReportErrors":"false","lang":"de","licenseKey":"PV3-E84EB78B-26EA-49EC-A938-2F269B752F89","licenseExpires":15967066053500,"licenseToken":"espSo+P57BSGiaYVZ8Ie9nR5jet1wBCu7cu6FcJGuSgLOqb87kSKeSY3fB+q+6tMktkB1HWF8JjmCBwnKGUSgQHDENon8E72mr413LDcCDY=","startPage":"visually-combine-reorder-pdf.ejs","splitByPages-outputPattern":"[CURRENTPAGE]-[BASENAME]","proxyServer":"","proxyUsr":"","proxyPwd":"","outputFolder":"SRC"}' > ~/.config/PDFsam\ Visual/pvp.pref
 
 cd ..
 rm -rf ./apptmp
